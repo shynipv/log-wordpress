5c66f51c771f528bf87ac7cf066ea5e9056b09b4
shynipv <57441362+shynipv@users.noreply.github.com>
2019-12-27 12:49:14 +0530

    Add files via upload

---
+++ log/content.php
@@ -0,0 +1,18 @@
+<div class="blog-post">
+  <h2 class="blog-post-title">Sign in</h2>
+  <p class="blog-post-meta">January 1, 2019 by <a href="#">asdf</a></p>
+
+  <form method="POST">
+    <p><label for="username">Username/Email</label>
+      <input type="text" id="username" name="username" placeholder="Enter Username/Email">
+    </p>
+    <p><label for="password">Password</label>
+      <input type="text" id="password" name="password" placeholder="Enter password">
+    </p>
+    <button type="submit" name="submit">Login</button>
+  </form>
+  <hr />
+
+  <!-- the rest of the content -->
+</div>
+<!-- /.blog-post -->
---
+++ log/footer.php
@@ -0,0 +1,22 @@
+</div> <!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Blog template built for <a href="http://getbootstrap.com">asdf</a> by <a href="https://twitter.com/mdo">@md</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+      <div class="sidebar-module">
+
+        <ol class="list-unstyled">
+          <li><a href="#">GitHub</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Facebook</a></li>
+        </ol>
+      </div>ss
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <?php wp_footer(); ?>
+  </body>
+</html>
